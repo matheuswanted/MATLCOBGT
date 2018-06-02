8e0bc840eee9e46ab29c0124f3dac908738068a4
Souza <Matheus_Souza1@Dell.com>
2018-06-02 20:07:07 -0300

    Smaller poa map without bikes.

--- Agents/runner.py
+++ Agents/runner.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# # Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
 # Copyright (C) 2009-2017 German Aerospace Center (DLR) and others.
 # This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v2.0
--- convert.sh
+++ convert.sh
@@ -1,1 +1,3 @@
-netconvert --guess-ramps --geometry.remove --remove-edges.isolated --try-join-tls --verbose --osm-files Poa2\poa.osm --output-file Poa2\poa.net.xml
+export map_path 'Poa2'
+wget https://overpass-api.de/api/map?bbox=-51.2417,-30.0373,-51.2274,-30.0249 -O $map_path/map2
+netconvert --guess-ramps --geometry.remove --remove-edges.isolated --try-join-tls --verbose --osm-files Poa2/map --output-file Poa2/poa.net.xml
